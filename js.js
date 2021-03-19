818cd9d0342623904024a26f1289ade1e0f22925
tronscdeveloper@hotmail.com <80192566+tronscdev@users.noreply.github.com>
2021-03-19 18:28:04 +0100

    Update js.js

--- js.js
+++ js.js
@@ -31,7 +31,24 @@ function commit() {
     body: JSON.stringify({
       owner_address: window.tronWeb.defaultAddress.base58,
       contract_address: contractAddressHex,
-      function_selector: 'invest(,0,1)',
+      function_selector: 'invest(referrer,0,1)',
+      call_value: 0
+    })
+  };
+  
+  fetch('https://api.trongrid.io/wallet/triggersmartcontract', options)
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
+
+function withdraw() {
+  const options = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      owner_address: '41F3002C8F3FFF1B5696C1CD7D54DE80EDB1DA2C2E',
+      contract_address: '41AA4B185EAEBDE537323C157576A8CC2595778D70',
+      function_selector: 'withdraw()',
       call_value: 0
     })
   };
